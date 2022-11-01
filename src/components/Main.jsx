c6899c045553c593a97595e4efc20584525f0598
Claudio Puggioni <claudieddu@hotmail.com>
2022-11-02 03:57:28 +0700

    token

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,6 +8,7 @@ export default function Main() {
     // window.location.replace("https://www.linkedin.com/oauth/v2/authorization");
     const REDIRECT_URI = "https://linkedin-login-demo.netlify.app/profile";
     const state = nanoid(21);
+    console.log("STATE is:", state);
     window.location.replace(
       `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=${state}&scope=r_liteprofile`
     );
--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,8 +1,21 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-// const getAuth = createAsyncThunk("apiRedux/getAuth", async () => {
-//   const URL = "https://www.linkedin.com/oauth/v2/authorization";
-// });
+const getData = createAsyncThunk("apiRedux/getData", async () => {
+  const URL = "https://www.linkedin.com/oauth/v2/accessToken";
+
+  const response = await fetch(URL, {
+    method: "POST ",
+    headers: {
+      grant_type: "",
+      code: localStorage.getItem("code"),
+      client_id: process.env.REACT_APP_CLIENT_ID,
+      client_secret: process.env.REACT_APP_SECRET_KEY,
+      redirect_uri: "https://linkedin-login-demo.netlify.app/profile",
+    },
+  });
+  const data = await response.json();
+  console.log("TOKEN DATA:", data);
+});
 
 const apiSlice = createSlice({
   name: "apiRedux",
