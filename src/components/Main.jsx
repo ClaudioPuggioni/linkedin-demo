2bc3e02b079562279d5fee41865022051125f2d9
Claudio Puggioni <claudieddu@hotmail.com>
2022-11-02 03:37:40 +0700

    profile page operational

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-// import { nanoid } from "@reduxjs/toolkit";
+import { nanoid } from "@reduxjs/toolkit";
 import React from "react";
 
 export default function Main() {
@@ -6,11 +6,11 @@ export default function Main() {
     // console.log(process.env.REACT_APP_CLIENT_ID);
     // console.log(process.env.REACT_APP_SECRET_KEY);
     // window.location.replace("https://www.linkedin.com/oauth/v2/authorization");
-    // const REDIRECT_URI = "https://linkedin-login-demo.netlify.app/profile";
-    // const state = nanoid(21);
-    // window.location.replace(
-    //   `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=${state}&scope=r_liteprofile`
-    // );
+    const REDIRECT_URI = "https://linkedin-login-demo.netlify.app/profile";
+    const state = nanoid(21);
+    window.location.replace(
+      `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=${state}&scope=r_liteprofile`
+    );
   };
 
   return (
